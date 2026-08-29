04c8b4493e60d1ee182f59d6ace40e8e6a1bf2b0
Developer <dev@example.com>
2026-08-29 15:37:00 +0000

    Complete missing features: persistence, Tabs UI, module.json5, icons, hvigor build

---
+++ entry/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+  system: hapTasks,  /* Built-in plugin of hvigor. Do not modify. */
+  plugins: []        /* Custom plugin. */
+}
